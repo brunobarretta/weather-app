dca66151568d05baf2f351bd0a10513ad4426aef
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-12-18 22:29:48 -0300

    convert api icons

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -9,20 +9,50 @@ import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
 
+const iconMappings:any = {
+  "11d": rain_icon,
+  "10d": rain_icon,
+  "09d": drizzle_icon,
+  "02d": cloud_icon,
+  "02n": cloud_icon,
+  "03d": cloud_icon,
+  "03n": cloud_icon,
+  "04d": cloud_icon,
+  "04n": cloud_icon,
+  "01d": clear_icon,
+  "01n": clear_icon,
+  "13d": snow_icon,
+};
+
 const Weather = () => {
   const [search, setSearch] = useState<string>("")
+  const [city, setCity] = useState<string>("")
+  const [icon, setIcon] = useState<any>(cloud_icon)
+  const [weather, setWeather] = useState<string>("")
+  const [humidity, setHumidity] = useState<string>("")
+  const [wind, setWind] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false)
 
   let api_key = "f4f48f2f9cdf818da9362c1a8a01cc5c"
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${api_key}&units=metric`
 
   const getWeather = async () => {
-
+    setLoading(true)
     let response = await fetch(url)
     let data = await response.json()
 
     console.log(data)
+
+    setCity(data.name)
+    setIcon(iconMappings[data.weather[0].icon] || cloud_icon)   
+    setWeather((data.main.temp.toFixed(0)) + "°")
+    setHumidity(data.main.humidity)
+    setWind(data.wind.speed)
+
+    // setLoading(false)
   }
+
 
   return (
     <div className='container'>
@@ -33,22 +63,22 @@ const Weather = () => {
         </div>
       </div>
       <div className='weather-image'>
-        <img src={cloud_icon} alt=''/>
+        <img src={icon} alt=''/>
       </div>
-      <div className='weather-temp'>24c</div>
-      <div className='weather-location'>London</div>
+      <div className='weather-temp'>{weather || "-"}</div>
+      <div className='weather-location'>{city || "-"}</div>
       <div className='data-container'>
         <div className='element'>
           <img src={humidity_icon} alt='' className='icon'/>
           <div className='data'>
-            <div className='humidity-percent'>64%</div>
+            <div className='humidity-percent'>{humidity || "- "}%</div>
             <div className='text'>Humidade</div>
           </div>
         </div>
         <div className='element'>
           <img src={wind_icon} alt='' className='icon'/>
           <div className='data'>
-            <div className='humidity-percent'>18 km/h</div>
+            <div className='humidity-percent'>{wind || "- "}km/h</div>
             <div className='text'>Velocidade do Vento</div>
           </div>
         </div>
