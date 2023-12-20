dff29324238abb30d20d616edf00800bd70b9fff
Bruno Barretta <bruno.barretta@fatec.sp.gov.br>
2023-12-19 21:54:03 -0300

    change styles

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,33 +1,13 @@
 import { useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
-import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
-import drizzle_icon from '../assets/drizzle.png'
-import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
-
-const iconMappings:any = {
-  "11d": rain_icon,
-  "10d": rain_icon,
-  "09d": drizzle_icon,
-  "02d": cloud_icon,
-  "02n": cloud_icon,
-  "03d": cloud_icon,
-  "03n": cloud_icon,
-  "04d": cloud_icon,
-  "04n": cloud_icon,
-  "01d": clear_icon,
-  "01n": clear_icon,
-  "13d": snow_icon,
-};
 
 const Weather = () => {
   const [search, setSearch] = useState<string>("")
   const [city, setCity] = useState<string>("")
-  const [icon, setIcon] = useState<any>(cloud_icon)
+  const [icon, setIcon] = useState<any>("01d")
   const [weather, setWeather] = useState<string>("")
   const [humidity, setHumidity] = useState<string>("")
   const [wind, setWind] = useState<string>("")
@@ -37,6 +17,7 @@ const Weather = () => {
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${api_key}&units=metric`
 
+
   const getWeather = async () => {
     setLoading(true)
     let response = await fetch(url)
@@ -45,41 +26,43 @@ const Weather = () => {
     console.log(data)
 
     setCity(data.name)
-    setIcon(iconMappings[data.weather[0].icon] || cloud_icon)   
+    setIcon(data.weather[0].icon)   
     setWeather((data.main.temp.toFixed(0)) + "°")
     setHumidity(data.main.humidity)
-    setWind(data.wind.speed)
+    setWind(data.wind.speed.toFixed(0))
 
-    // setLoading(false)
+    setLoading(false)
   }
 
 
   return (
     <div className='container'>
-      <div className='top-bar'>
-        <input type='text' className='cityInput' placeholder='Pesquise uma cidade' value={search} onChange={(e) => setSearch(e.target.value)}/>
-        <div className='search-icon' onClick={() => getWeather()}>
-          <img src={search_icon} alt=''/>
-        </div>
-      </div>
-      <div className='weather-image'>
-        <img src={icon} alt=''/>
-      </div>
-      <div className='weather-temp'>{weather || "-"}</div>
-      <div className='weather-location'>{city || "-"}</div>
-      <div className='data-container'>
-        <div className='element'>
-          <img src={humidity_icon} alt='' className='icon'/>
-          <div className='data'>
-            <div className='humidity-percent'>{humidity || "- "}%</div>
-            <div className='text'>Humidade</div>
+      <div className='card-weather'>
+        <div className='top-bar'>
+          <input type='text' className='cityInput' placeholder='Pesquise uma cidade' value={search} onChange={(e) => setSearch(e.target.value)}/>
+          <div className='search-icon' onClick={() => getWeather()}>
+            <img src={search_icon} alt=''/>
           </div>
         </div>
-        <div className='element'>
-          <img src={wind_icon} alt='' className='icon'/>
-          <div className='data'>
-            <div className='humidity-percent'>{wind || "- "}km/h</div>
-            <div className='text'>Velocidade do Vento</div>
+        <div className='weather-image'>
+          <img src={`https://openweathermap.org/img/wn/${icon}@4x.png`} alt='' className='icon-image'/>
+        </div>
+        <div className='weather-temp'>{weather || "-"}</div>
+        <div className='weather-location'>{city || "-"}</div>
+        <div className='data-container'>
+          <div className='element'>
+            <img src={humidity_icon} alt='' className='icon'/>
+            <div className='data'>
+              <div className='humidity-percent'>{humidity || "- "}%</div>
+              <div className='text'>Humidade</div>
+            </div>
+          </div>
+          <div className='element'>
+            <img src={wind_icon} alt='' className='icon'/>
+            <div className='data'>
+              <div className='humidity-percent'>{wind || "- "}km/h</div>
+              <div className='text'>Velocidade do Vento</div>
+            </div>
           </div>
         </div>
       </div>
